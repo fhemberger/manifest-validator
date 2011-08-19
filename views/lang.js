c936ed14b7ec8de1c7d6f9566aa838b191c7a523
fhemberger <mail@frederic-hemberger.de>
2011-08-19 10:13:52 +0200

    Improved error checking on resources

--- views/lang.js
+++ views/lang.js
@@ -4,6 +4,7 @@ module.exports = {
 	ERR_INVALID_FILE      : 'Invalid file type: Only text files allowed.',
 	ERR_LOAD_FILE         : 'Error loading uploaded manifest file.',
 	ERR_EMPTY_FILE        : 'Could not validate manifest: File is empty.',
+	ERR_RESOURCE_ERROR    : 'Error parsing resource',
 	ERR_MANIFEST_MIMETYPE : 'Server must deliver cache manifest with the MIME type "text/cache-manifest".',
 	ERR_MANIFEST_HEADER   : 'Cache manifest must start with CACHE MANIFEST in first line.',
 	ERR_MANIFEST_INVALID_RESOURCE : 'Invalid resource identifier.',
