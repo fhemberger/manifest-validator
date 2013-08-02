880c62154a08f59d5d4b41ba551059160ba9069c
Frederic Hemberger <mail@frederic-hemberger.de>
2013-08-02 21:59:05 +0200

    Suport graceful shutdown on SIGTERM

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(express.compress());
 app.use(express.favicon(__dirname + '/public/favicon.ico', { maxAge: oneDayInMilliseconds * 30 }));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
-app.use(express.methodOverride());
 app.use(express.static(__dirname + '/public', { maxAge: oneDayInMilliseconds }));
 app.use(app.router);
 
@@ -46,6 +45,19 @@ app.get( '/',             routes.html.index);
 app.get( '*',             routes.html.error404);
 
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server in \x1B[1m\x1B[31m' + app.get('env') + '\x1B[39m, listening on port \x1B[1m\x1B[31m' + app.get('port') + '\x1B[39m');
+});
+
+process.on('SIGTERM', function() {
+  console.log('Received kill signal (SIGTERM), shutting down gracefully.');
+  server.close(function() {
+    console.log('Closed out remaining connections.');
+    process.exit();
+  });
+
+  setTimeout(function() {
+    console.error('Could not close connections in time, forcefully shutting down');
+    process.exit(1);
+  }, 30000);
 });
