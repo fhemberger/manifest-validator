d8ae672c2ccdf074569087277fbf18d9f9273f48
Frederic Hemberger <mail@frederic-hemberger.de>
2014-01-10 14:53:51 +0100

    Be nice to jshint

--- test/analytics.test.js
+++ test/analytics.test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var should    = require('chai').should(),
     analytics = require('../lib/analytics.js');
 
--- test/manifest.test.js
+++ test/manifest.test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var should   = require('chai').should(),
     Manifest = require('../lib/manifest.js');
 
