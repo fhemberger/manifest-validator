22ffaf322d120a53c07c22f99b35f75bf0b0c935
Frederic Hemberger <mail@frederic-hemberger.de>
2014-03-07 16:37:16 +0100

    Extract application config values
    Additionally, I switched to YAML for configuration. It may sound trivial, but I really love the ability to add comments. On top of this, the syntax is much cleaner.

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
 'use strict';
 
 var express = require('express'),
-    app = express();
+    yaml    = require('./lib/require-yaml'),
+    config  = require('./lib/config.js'),
+    app     = express();
 
 var routes = {
     html: require('./routes/html.js'),
     api : require('./routes/api.js')
 };
 
-var oneDayInMilliseconds = 86400000;
-
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('env', process.env.NODE_ENV || 'development');
+app.set('port', config.express.port);
+app.set('env', config.env);
+app.set('baseurl', config.express.baseurl);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
+
+app.locals = {
+  lang   : yaml('./config/messages.yml'),
+  config : config
+};
+
+// TODO: All middleware will be removed from Express 4.0, must be added to package.json separately
 app.use(express.compress());
-app.use(express.favicon(__dirname + '/public/favicon.ico', { maxAge: oneDayInMilliseconds * 30 }));
-app.use(express.static(__dirname + '/public', { maxAge: oneDayInMilliseconds }));
-app.use(app.router);
-app.locals.lang = require('./config/messages.json');
+app.use(express.favicon(__dirname + '/public/favicon.ico', { maxAge: config.express.caching.favicon }));
+app.use(express.static(__dirname + '/public', { maxAge: config.express.caching.static }));
 app.disable('x-powered-by');
 
 
-app.configure('development', function() {
-  app.set('baseurl', 'http://localhost:' + app.get('port'));
-  app.use(express.logger('dev'));
-  app.use(express.errorHandler());
-});
+if (config.env === 'development') {
+    app.use(express.logger('dev'));
+    app.use(express.errorHandler());
+}
 
-
-app.configure('production', function() {
-  app.set('baseurl', 'http://manifest-validator.com');
-  app.use(express.logger());
-  app.set('view cache');
-});
+if (config.env === 'production') {
+    app.use(express.logger());
+    app.set('view cache');
+}
 
 
 // API
@@ -44,7 +47,6 @@ app.post('/api/validate', routes.api.validatePOST);
 
 
 // HTML
-// Don't call the result page directly
 app.get( '/validate',     function(req, res) { res.redirect('/'); });
 app.post('/validate',     routes.html.validate);
 app.get( '/',             routes.html.index);
@@ -63,4 +65,3 @@ require('./lib/graceful-shutdown.js')(server);
 
 // Expose application instance for testing
 module.exports = app;
-
